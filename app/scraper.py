965d661a4250e959642cec7183b92408cbf58849
Nebhg <benharasgummer@gmail.com>
2024-03-06 16:38:56 +0000

    changed command executtor and fixed issues with selenium

--- app/scraper.py
+++ app/scraper.py
@@ -10,7 +10,7 @@ proxy = "http://customer-AkaThePistachio:Kobe2601202o@pr.oxylabs.io:7777"
 def set_chrome_options():
     """Sets Chrome options for Selenium."""
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(f"--proxy-server={proxy}")
@@ -20,19 +20,14 @@ def scrape_website():
     """Scrapes the given URL and returns the IP address."""
     chrome_options = set_chrome_options()
     driver = webdriver.Remote(
-        command_executor='http://selenium-server:4444/wd/hub', # Replace 'selenium-server' with the appropriate address of your Selenium hub
+        command_executor='http://172.23.0.2:4444', # Replace 'selenium-server' with the appropriate address of your Selenium hub
         options=chrome_options
     )
     try:
         driver.get("https://httpbin.org/ip")
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'token-string'))
-        )
-        ip_element = driver.find_element(By.CLASS_NAME, 'token-string')
-        ip_address = ip_element.text.strip('"')
-        return {"IP Address": ip_address}
+       
+        return {"Sucessful connection": driver.current_url}
     except Exception as e:
-        driver.save_screenshot('error.png')  # Save a screenshot on error
         print(f"An error occurred: {e}")
         return {"error": str(e)}
     finally:
