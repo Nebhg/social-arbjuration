2c749377180d8825dce60076e9dffba5bd765693
Nebhg <benharasgummer@gmail.com>
2024-03-07 02:33:38 +0000

    Able to scrape basic pages

--- app/scraper.py
+++ app/scraper.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import json
 
-proxy = "http://customer-AkaThePistachio:Kobe2601202o@pr.oxylabs.io:7777"
+proxy = "http://pr.oxylabs.io:7777"
 
 def set_chrome_options():
     """Sets Chrome options for Selenium."""
@@ -24,9 +24,17 @@ def scrape_website():
         options=chrome_options
     )
     try:
-        driver.get("https://httpbin.org/ip")
-       
-        return {"Sucessful connection": driver.current_url}
+        url="https://stackoverflow.com/questions/67871777/why-am-i-getting-an-empty-body-tag-content-when-trying-to-use-web-scraping-using"
+        driver.get(url)
+        #Wait for page to load
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "body"))
+        )
+        page_source = driver.page_source
+
+        soup = BeautifulSoup(page_source, "html.parser")
+        body_content = soup.find('body').get_text(strip=True)
+        return {"HTML Content": body_content}
     except Exception as e:
         print(f"An error occurred: {e}")
         return {"error": str(e)}
